40bcccc40f1e62da517baacf66e98fb41e06e79a
Ahnaf Sakil <55395335+ahnaf16@users.noreply.github.com>
2025-11-22 15:30:19 +0600

    logout

--- lib/features/home/view/home_view.dart
+++ lib/features/home/view/home_view.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:linkify/features/auth/controller/auth_ctrl.dart';
 import 'package:linkify/main.export.dart';
+import 'package:pull_down_button/pull_down_button.dart';
 
 class HomeView extends HookConsumerWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final authCtrl = useMemoized(() => ref.read(authCtrlProvider.notifier));
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(kAppName),
         actions: [
-          CircleAvatar(backgroundColor: context.colors.primary, child: const Icon(Icons.person_rounded)),
+          PopOver(
+            itemBuilder: (context) => [
+              const PullDownMenuTitle(title: Text('Guest User')),
+              PullDownMenuItem(onTap: () {}, title: 'Dark Theme', icon: Icons.nightlight_round),
+              PullDownMenuItem(onTap: () {}, title: 'Settings', icon: Icons.settings_rounded),
+              PullDownMenuItem(
+                onTap: () => authCtrl.logout(),
+                title: 'Logout',
+                icon: Icons.logout_rounded,
+                isDestructive: true,
+              ),
+            ],
+            buttonBuilder: (context, showMenu) => CircleAvatar(
+              backgroundColor: context.colors.primary,
+              child: const Icon(Icons.person_rounded),
+            ).clickable(onTap: showMenu),
+          ),
           const Gap(Insets.sm),
         ],
       ),
